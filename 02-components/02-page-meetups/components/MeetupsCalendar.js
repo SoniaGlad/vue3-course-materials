a75f1491d7948b69b00c3e4aac54f61333d9a873
Sofya Gladkikh <sg@soniaglad.com>
2022-02-03 23:30:21 +0300

    UiButtonGroup added; PageMeetups updated

---
+++ 02-components/02-page-meetups/components/MeetupsCalendar.js
@@ -0,0 +1,15 @@
+import { createApp, defineComponent } from '../vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupsCalendar',
+
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  template: `
+    <div class='meetups-calendar'>Календарь</div>`,
+});
--- 02-components/02-page-meetups/components/PageMeetups.js
+++ 02-components/02-page-meetups/components/PageMeetups.js
@@ -1,5 +1,7 @@
 import { createApp, defineComponent } from '../vendor/vue.esm-browser.js';
 import MeetupsList from './MeetupsList.js';
+import MeetupsCalendar from './MeetupsCalendar.js';
+import UiButtonGroup from './UiButtonGroup.js';
 
 const fetchMeetups = () => fetch('./api/meetups.json').then((res) => res.json());
 
@@ -8,6 +10,8 @@ export default defineComponent({
 
   components: {
     MeetupsList,
+    MeetupsCalendar,
+    UiButtonGroup,
   },
 
   data() {
@@ -108,40 +112,7 @@ export default defineComponent({
           </div>
         </div>
         <div class='form-group form-group_inline'>
-          <div class='button-group' role='group'>
-            <button
-              type='button'
-              class='button-group__button'
-              :class="{ 'button-group__button_active': view === 'list' }"
-              aria-label='Список'
-              :aria-selected="view === 'list'"
-              @click="view = 'list'"
-            >
-              <svg fill='none' height='28' viewBox='0 0 28 28' width='28' xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  clip-rule='evenodd'
-                  d='m5 7c-.55228 0-1 .44772-1 1s.44772 1 1 1h.01c.55228 0 1-.44772 1-1s-.44772-1-1-1zm5 0c-.55228 0-1 .44772-1 1s.44772 1 1 1h13c.5523 0 1-.44772 1-1s-.4477-1-1-1zm0 6c-.55228 0-1 .4477-1 1s.44772 1 1 1h13c.5523 0 1-.4477 1-1s-.4477-1-1-1zm-1 7c0-.5523.44772-1 1-1h13c.5523 0 1 .4477 1 1s-.4477 1-1 1h-13c-.55228 0-1-.4477-1-1zm-5-6c0-.5523.44772-1 1-1h.01c.55228 0 1 .4477 1 1s-.44772 1-1 1h-.01c-.55228 0-1-.4477-1-1zm1 5c-.55228 0-1 .4477-1 1s.44772 1 1 1h.01c.55228 0 1-.4477 1-1s-.44772-1-1-1z'
-                  fill-rule='evenodd'
-                ></path>
-              </svg>
-            </button>
-            <button
-              type='button'
-              class='button-group__button'
-              :class="{ 'button-group__button_active': view === 'calendar' }"
-              aria-label='Календарь'
-              :aria-selected="view === 'calendar'"
-              @click="view = 'calendar'"
-            >
-              <svg height='22' viewBox='0 0 20 22' width='20' xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  clip-rule='evenodd'
-                  d='m15 1c0-.552285-.4477-1-1-1s-1 .447715-1 1v1h-6v-1c0-.552285-.44772-1-1-1s-1 .447715-1 1v1h-2c-1.65685 0-3 1.34315-3 3v14c0 1.6569 1.34315 3 3 3h14c1.6569 0 3-1.3431 3-3v-14c0-1.65685-1.3431-3-3-3h-2zm3 7v-3c0-.55228-.4477-1-1-1h-2v1c0 .55228-.4477 1-1 1s-1-.44772-1-1v-1h-6v1c0 .55228-.44772 1-1 1s-1-.44772-1-1v-1h-2c-.55228 0-1 .44772-1 1v3zm-16 2h16v9c0 .5523-.4477 1-1 1h-14c-.55228 0-1-.4477-1-1z'
-                  fill-rule='evenodd'
-                ></path>
-              </svg>
-            </button>
-          </div>
+          <ui-button-group :view='view' @select='view = $event' />
         </div>
       </div>
     </div>
@@ -149,11 +120,17 @@ export default defineComponent({
     <template v-if='meetups'>
       <template v-if='filteredMeetups.length'>
         <meetups-list v-if='view === "list"' :meetups='filteredMeetups' />
-        <div v-else-if="view === 'calendar'" class='meetups-calendar'>Календарь</div>
+        <meetups-calendar v-else-if="view === 'calendar'" :meetups='filteredMeetups' />
       </template>
       <div v-else class='alert'>Митапов по заданным условиям не найдено...</div>
     </template>
     <div v-else class='alert'>Загрузка...</div>
     </div>
-  `,
+    <script>
+    import MeetupsCalendar from './MeetupsCalendar';
+
+    export default {
+      components: { MeetupsCalendar },
+    };
+    </script>`,
 });
